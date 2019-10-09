9d40cf203d77a55c62dce6b904b1574fcea95320
Caleb <caleb.falcione@gmail.com>
2019-10-09 12:11:42 -0500

    shows which chords intersect instead of merely that some chords intersect

--- src/app/lamination-viewer/lamination-viewer.component.ts
+++ src/app/lamination-viewer/lamination-viewer.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
-import { Polygon, NaryFraction } from 'laminations-lib';
+import { Polygon, NaryFraction, Chord } from 'laminations-lib';
 import { makeCanvasRenderer } from '../../lib/lamination-renderer/canvas-renderer';
 import { RenderSettings, LaminationState } from 'src/lib/definitions';
 
+type Intersection = [Chord, Chord]
 
 @Component({
   selector: 'app-lamination-viewer',
@@ -17,7 +18,7 @@ export class LaminationViewerComponent implements OnInit {
 
   @ViewChild('laminationCanvas') canvas: ElementRef
 
-  hasIntersections = false
+  intersections: Intersection[] = []
 
   constructor() { }
 
@@ -27,10 +28,10 @@ export class LaminationViewerComponent implements OnInit {
     const ctx: CanvasRenderingContext2D = this.canvas.nativeElement.getContext('2d')
     const renderer = makeCanvasRenderer(ctx, this.settings)
     renderer.render(this.laminationState)
-    this.hasIntersections = this.checkForIntersections()
+    this.intersections = [...this.checkForIntersections()]
   }
 
-  checkForIntersections(): boolean {
+  *checkForIntersections(): IterableIterator<Intersection> {
     // TODO: we can do better than O(n^2).
     const lamination = this.laminationState.lamination
 
@@ -42,18 +43,23 @@ export class LaminationViewerComponent implements OnInit {
     for (let i = 0; i < chords.length - 1; i++) {
       for (let j = i + 1; j < chords.length; j++) {
         if (chords[i].intersects(chords[j])) {
-          return true
+          yield [chords[i], chords[j]]
         }
       }
     }
-    return false
   }
 
-  prettyPrint(lamination: Polygon[]) {
+  prettyPrintLamination(lamination: Polygon[]) {
     return lamination
       .sort((a, b) => NaryFraction.compare(a.points[0], b.points[0]))
       .map(poly => `${poly}`)
       .join("\n")
+  }
+
+  prettyPrintIntersections(intersections: Intersection[]) {
+    return intersections
+    .map(([chordA, chordB]) => `${chordA} -- ${chordB}`)
+    .join("\n")
   }
 
 }
