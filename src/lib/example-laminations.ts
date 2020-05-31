cd5f327608b4ec18bf59d4eb113f963dfb654727
Caleb Falcione <caleb.falcione@gmail.com>
2020-05-30 19:27:35 -0500

    removes unnecessary indentation in example json

--- src/lib/example-laminations.ts
+++ src/lib/example-laminations.ts
@@ -45,47 +45,48 @@ export const never_close_quintary_def: LaminationDefinition = {
 }
 
 export const template =
-  `{
-    "name": "EXAMPLE LAMINATION",
-    "base": 3,
-    "description": "A sample lamination that exercises the lion's share of available options. Any omitted optional parameters will receive a default value. Note the optional settings overrides and variety of ways to specify color.",
-    "branches": [
-      {
-        // Line comments are supported.
-        "chord": ["_002", "2_020"],
-        "endpoints": ["2_020"], // trailing commas are acceptable
-      },
-      {
-        "chord": ["_101", "2_011"],
-        "endpoints": ["2_011"],
-      }
-    ],
-    "leaves": [
-      {"points": ["_002", "_101", "_201"]},
-      {
-        "points": ["_011", "_020", "_012"],
-        "settings": {
-          "fillColor": "#FF0000"
-        },
-      },
-      {"points": ["_110", "_200", "_120"]},
-      {"points": ["_110"], "settings": {"strokeWidth": 6, /*"fillColor": "black"*/}}, // block comments are supported too
-    ],
-    "settings": {
-      "size": 700,
-      // "renderHyperbolic": true,
-      "criticalChords": {
-        "strokeWidth": 2
-      },
-      "polygons": {
-        "fillColor": "rgb(0, 100, 0)"
-      },
-      "circle": {
-        "fillColor": "rgb(205, 205, 205)",
-        "strokeColor": "black",
-        "strokeWidth": 5,
-      },
+  `\
+{
+  "name": "EXAMPLE LAMINATION",
+  "base": 3,
+  "description": "A sample lamination that exercises the lion's share of available options. Any omitted optional parameters will receive a default value. Note the optional settings overrides and variety of ways to specify color.",
+  "branches": [
+    {
+      // Line comments are supported.
+      "chord": ["_002", "2_020"],
+      "endpoints": ["2_020"], // trailing commas are acceptable
+    },
+    {
+      "chord": ["_101", "2_011"],
+      "endpoints": ["2_011"],
     }
-  }  
+  ],
+  "leaves": [
+    {"points": ["_002", "_101", "_201"]},
+    {
+      "points": ["_011", "_020", "_012"],
+      "settings": {
+        "fillColor": "#FF0000"
+      },
+    },
+    {"points": ["_110", "_200", "_120"]},
+    {"points": ["_110"], "settings": {"strokeWidth": 6, /*"fillColor": "black"*/}}, // block comments are supported too
+  ],
+  "settings": {
+    "size": 700,
+    // "renderHyperbolic": true,
+    "criticalChords": {
+      "strokeWidth": 2
+    },
+    "polygons": {
+      "fillColor": "rgb(0, 100, 0)"
+    },
+    "circle": {
+      "fillColor": "rgb(205, 205, 205)",
+      "strokeColor": "black",
+      "strokeWidth": 5,
+    },
+  }
+}  
 `
 
